16f0e95fbbb5658de993d8b553934f99829155a9
Hyun Park <hyunp.dev@gmail.com>
2025-03-21 13:48:04 +0900

    Update save.py

--- save.py
+++ save.py
@@ -8,7 +8,7 @@ import requests
 
 
 def main():
-    print("Generating ics file as output/illustar.ics)
+    print("Generating ics file as output/illustar.ics")
     if not os.path.exists("output"):
         os.mkdir("output")
     cal = vobject.iCalendar()
