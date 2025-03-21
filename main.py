877dafa2740a763ff7b8d2e2541690df06100ba2
Hyun Park <hyunp.dev@gmail.com>
2025-03-21 12:33:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,25 @@ async def download_ical():
         "Content-Disposition": "attachment; filename=illustar.ics",
     }
     cal = vobject.iCalendar()
-    res = requests.get("https://api.illustar.net/v1/concert?row_per_page=30&page=1&keyword=")
-    data = json.loads(zlib.decompress(bytes(res.json()["data"].values())))
-    for event in data["list"]:
+    
+    concert_res = requests.get("https://api.illustar.net/v1/concert?row_per_page=30&page=1&keyword=")
+    concert_data = json.loads(zlib.decompress(bytes(concert_res.json()["data"].values())))["list"]
+
+    event_res = requests.get("https://api.illustar.net/v1/event/list")
+    event_data = json.loads(zlib.decompress(bytes(event_res.json()["data"].values())))["eventInfo"]
+
+    for event in concert_data + event_data:
+        if event["start_date"].startswith("0"):
+            continue
+        
         vevent = cal.add("vevent")
         vevent.add("summary").value = event["name"]
         vevent.add("location").value = event["place"]
+        
         vevent.add('dtstart').value = datetime.fromisoformat(event["start_date"])
         vevent.add('dtend').value = datetime.fromisoformat(event["end_date"])
+    
+    
     return StreamingResponse(BytesIO(cal.serialize().encode("utf-8")),
                              headers=headers,
                              media_type="text/calendar; charset=utf-8")
